a239c3d757320c003fbf09b4dcff5741e1cc02d4
Marcos Aleixo <smartrhvoue@gmail.com>
2026-03-16 22:55:00 -0300

    160220265

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,24 +2,45 @@ import { useState } from "react";
 import "../styles/navbar.css";
 import mockup from "../assets/logo-3.png";
 import { openPlayStore } from "../utils/appLinks";
+
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+
+  function handleLinkClick() {
+    setOpen(false);
+  }
 
   return (
     <nav className="navbar">
       <div className="container nav-content">
-        {/* <div className="logo">APPRIX</div> */}
         <div className="logo-wrapper">
-          <img className="logo-image" src={mockup} alt="Apprix" sizes={40} />
+          <img className="logo-image" src={mockup} alt="Apprix" />
         </div>
 
         <div className={`nav-links ${open ? "active" : ""}`}>
-          <a href="#features">Funcionalidades</a>
-          <a href="#how">Como funciona</a>
-          <a href="#pricing">Planos</a>
-          <a href="#faq">FAQ</a>
+          <a href="#features" onClick={handleLinkClick}>
+            Funcionalidades
+          </a>
 
-          <button className="btn-primary" onClick={openPlayStore}>
+          <a href="#how" onClick={handleLinkClick}>
+            Como funciona
+          </a>
+
+          <a href="#pricing" onClick={handleLinkClick}>
+            Planos
+          </a>
+
+          <a href="#faq" onClick={handleLinkClick}>
+            FAQ
+          </a>
+
+          <button
+            className="btn-primary"
+            onClick={() => {
+              openPlayStore();
+              setOpen(false);
+            }}
+          >
             Baixar App
           </button>
         </div>
